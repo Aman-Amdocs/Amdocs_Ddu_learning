a1c6423a039b3e1867edfa7dc1fd3b4197a1b783
Aman Solanki <144650209+Aman-Amdocs@users.noreply.github.com>
2023-09-11 10:41:06 +0530

    First Commit

---
+++ src/main/java/com/amdocs/commanality/ddu/learning/standalone/service/ValidateSkillApplicationService.java
@@ -0,0 +1,28 @@
+package com.amdocs.commanality.ddu.learning.standalone.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.amdocs.commanality.ddu.learning.standalone.model.EmployeeMissingSkillSet;
+import com.amdocs.commanality.ddu.learning.standalone.model.EmployeeSkillSet;
+
+@Service
+public class ValidateSkillApplicationService {
+	
+	@Autowired
+	EmployeeMissingSkillSet employeeMissingSkillSet;
+	
+	public EmployeeMissingSkillSet validateSkills(EmployeeSkillSet employeeSkillSet) {
+		
+		employeeMissingSkillSet.setEmpId(employeeSkillSet.getEmpId());
+		
+		employeeSkillSet.getRequiredSkills().removeIf(skill -> employeeSkillSet.getActualSkills().stream()
+				.anyMatch(skill2 -> skill2.equalsIgnoreCase(skill)));
+		
+		employeeMissingSkillSet.setMissingSkills(employeeSkillSet.getRequiredSkills());
+		
+		return employeeMissingSkillSet;
+		
+	}
+
+}
